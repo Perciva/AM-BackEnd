b5bd438a58918c5d82faf977babc291ed4862dca
Perciva <stromchild@gmail.com>
2021-01-02 11:08:55 +0700

    added jwt

--- app/app.py
+++ app/app.py
@@ -1,23 +1,31 @@
 from flask import Flask, request, jsonify
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 from ariadne import graphql_sync, make_executable_schema, QueryType, load_schema_from_path, ObjectType
 from ariadne.constants import PLAYGROUND_HTML
 from app.tools import color as c
 import app.resolver as r
 from app.database import init_db
 
+
 type = load_schema_from_path('./app/schema.graphql')
 
 q = QueryType()
 user = ObjectType('User')
-
+login = ObjectType('LoginData')
 q.set_field('hello', r.hello_resolver)
 q.set_field('GetUser', r.getUser)
 
-schema = make_executable_schema(type, [q,user])
+schema = make_executable_schema(type, [q,user,login])
 
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'secret'
+jwt = JWTManager(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/attendance'
 # init_db()
+
 
 @app.route('/')
 def index():
@@ -37,7 +45,6 @@ def graphql_server():
         context_value=request,
         debug=app.debug
     )
-
     status_code = 200 if success else 400
     return jsonify(result), status_code
 
--- app/resolver.py
+++ app/resolver.py
@@ -1,5 +1,11 @@
 import requests
+import json
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 
+@jwt_required
 def hello_resolver(_,info):
     print(info)
     return "Hello There!!!"
@@ -7,14 +13,22 @@ def hello_resolver(_,info):
 def getUser(_,info,username, password):
     URL = "https://laboratory.binus.ac.id/lapi/api/Account/LogOnQualification"
 
+    access_token = create_access_token(identity=username)
     d ={
         "username" : username,
         "password" : password
     }
 
     x = requests.post(URL,data = d)
-    return x.json()
-    
+    if(x.text == 'null'):
+        return 'null'
+    else :
+        resp = {
+            "UserData": x.json(),
+            "Token" :  access_token
+        }
+        return resp
+
 
 
 
