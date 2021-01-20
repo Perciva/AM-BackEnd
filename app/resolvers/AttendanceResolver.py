0099a1553869557093e4c016bfd99bf08bcc1fe7
Perciva <stromchild@gmail.com>
2021-01-20 11:41:42 +0700

    fixed syntac errr

--- app/resolvers/AttendanceResolver.py
+++ app/resolvers/AttendanceResolver.py
@@ -2,7 +2,7 @@ from app.model import attendance
 from app.database import query, mutation
 
 @mutation.field("InsertAttendance")
-def insertAttendance(_, info, assistant_initial, date, _in, _out)
+def insertAttendance(_, info, assistant_initial, date, _in, _out):
     return attendance.insert(assistant_initial, date, _in, _out)
 
 
