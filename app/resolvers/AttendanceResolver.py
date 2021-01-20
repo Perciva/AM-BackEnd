c7c54c286b3c10a01261fcf0e8f2d249d58839ff
Perciva <stromchild@gmail.com>
2021-01-20 11:37:49 +0700

    Fix Insert Attendance Resolver

--- app/resolvers/AttendanceResolver.py
+++ app/resolvers/AttendanceResolver.py
@@ -1,7 +1,9 @@
 from app.model import attendance
 from app.database import query, mutation
 
-
+@mutation.field("InsertAttendance")
+def insertAttendance(_, info, assistant_initial, date, _in, _out)
+    return attendance.insert(assistant_initial, date, _in, _out)
 
 
 
